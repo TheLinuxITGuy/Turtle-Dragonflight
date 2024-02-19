96b15aff9b2724acecb296553819906285a05ec9
TheLinuxITGuy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-02-18 20:20:15 -0700

    Update tCastbar.lua
    ancored the castbar to the action bar so it shows up correctly on different resolutions.

--- tDF/tCastbar.lua
+++ tDF/tCastbar.lua
@@ -1,8 +1,12 @@
 -- Create a tCastbar frame
 local tCastbar = CreateFrame("StatusBar", "CastBar", UIParent)
 tCastbar:SetWidth(200)
-tCastbar:SetHeight(20)
-tCastbar:SetPoint("CENTER", UIParent, "CENTER", 0, -425)
+tCastbar:SetHeight(10)
+--tCastbar:SetPoint("CENTER", UIParent, "CENTER", 0, -425)
+-- Get the position of ActionButton6
+local point, relativeTo, relativePoint, xOfs, yOfs = ActionButton6:GetPoint()
+-- Set the point of the tCastbar frame to be 4 pixels above ActionButton6
+tCastbar:SetPoint(point, relativeTo, relativePoint, xOfs - 50, yOfs + 175)
 tCastbar:SetStatusBarTexture("Interface\\AddOns\\tDF\\img\\Castbar\\CastingBarStandard2")
 tCastbar:SetStatusBarColor(0.8, 0.8, 0.8)
 
@@ -15,6 +19,7 @@ CastingBarFrame:Hide()
 local border = CreateFrame("Frame", nil, tCastbar)
 border:SetBackdrop({
   edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+  --edgeFile = "Interface\\AddOns\\tDF\\img\\Castbar\\CastingBarFrame2",
   edgeSize = 16,
   insets = { left = 4, right = 4, top = 4, bottom = 4 }
 })
@@ -24,11 +29,13 @@ border:SetPoint("BOTTOMRIGHT", tCastbar, 5, -5)
 
 local background = tCastbar:CreateTexture(nil, "BACKGROUND")
 background:SetTexture(0, 0, 0, 0.5)
+--background:SetTexture("Interface\\AddOns\\tDF\\img\\Castbar\\CastingBarBackground2")
 background:SetAllPoints(tCastbar)
 
 -- Create a text for the tCastbar
 local text = tCastbar:CreateFontString(nil, "OVERLAY")
-text:SetFont("Interface\\Addons\\tDF\\fonts\\Prototype.TTF", 12, "OUTLINE")
+text:SetFont(STANDARD_TEXT_FONT, 10, "OUTLINE")
+--text:SetFont("Interface\\Addons\\tDF\\fonts\\Prototype.TTF", 12, "OUTLINE")
 text:SetPoint("CENTER", tCastbar, "CENTER", 0, 0)
 text:SetTextColor(1, 1, 1)
 
@@ -37,7 +44,7 @@ local spark = tCastbar:CreateTexture(nil, "OVERLAY")
 spark:SetTexture("Interface\\AddOns\\tDF\\img\\Castbar\\CastingBarSpark")
 spark:SetBlendMode("ADD")
 spark:SetWidth(20)
-spark:SetHeight(40)
+spark:SetHeight(25)
 
 -- Register for events and update the tCastbar
 tCastbar:RegisterEvent("SPELLCAST_START")
