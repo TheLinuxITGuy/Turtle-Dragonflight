e859f0fb02d8db7d181bbf5d969c8599ff0a01a7
TheLinuxITGuy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-02-20 07:26:56 -0700

    Update tMinimap.lua
    Added Zoom In and Zoom Out buttons to the Minimap

--- tDF/tMinimap.lua
+++ tDF/tMinimap.lua
@@ -8,44 +8,48 @@ local tMinimapZoomOut = MinimapZoomOut
 local tMinimapBorderTop = MinimapBorderTop
 local tBuffButton0 = BuffButton0
 
---Set DF texture and scale to match DF size
-
+--Set Minimap to DF texture and scale to match DF size
 tMinimapBorder:SetTexture("Interface\\AddOns\\tDF\\img\\Dragonflight-MiniMap")
 --MinimapBorder:SetPoint("TOPLEFT", MinimapCluster, "TOPLEFT", 0, -40)
 tMinimapCluster:SetScale(1.4)
 tMinimapToggleButton:Hide()
 tGameTimeFrame:Hide()
---MinimapZoomIn:SetNormalTexture("Interface\\AddOns\\tDF\\img\\ZoomIn")
---MinimapZoomIn:SetPushedTexture()
---MinimapZoomIn:SetHighlightTexture()
---MinimapZoomOut:SetTexture("")
-tMinimapZoomIn:Hide()
-tMinimapZoomOut:Hide()
-tMinimapBorderTop:SetTexture()
+
+--Sets the ZoomIn and ZoomOut buttons
+--Normal
+tMinimapZoomIn:SetNormalTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32.tga")
+tMinimapZoomIn:SetWidth(16)
+tMinimapZoomIn:SetHeight(16)
+--Mouse over
+tMinimapZoomIn:SetHighlightTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32-over.tga")
+--Mouse click
+tMinimapZoomIn:SetPushedTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32-push.tga")
+
+tMinimapZoomOut:SetNormalTexture("Interface\\AddOns\\tDF\\img\\ZoomOut32.tga")
+tMinimapZoomOut:SetWidth(16)
+tMinimapZoomOut:SetHeight(16)
+--Mouse over
+tMinimapZoomOut:SetHighlightTexture("Interface\\AddOns\\tDF\\img\\ZoomOut32-push.tga")
+--Mouse click
+tMinimapZoomOut:SetPushedTexture("Interface\\AddOns\\tDF\\img\\ZoomOut32-push.tga")
+
 --Minimap:SetPoint("TOPLEFT", MinimapCluster, "BOTTOMLEFT", 0, -10)
-
---scoot buffbars to the left of the 1.4x minimap
-tBuffButton0:ClearAllPoints()
-tBuffButton0:SetPoint("TOPRIGHT", MinimapCluster, "TOPLEFT", 10, -10)
-
-
 --[[
--- Create a button frame
-local zoomInButton = CreateFrame("Button", "MyZoomInButton", Minimap, "UIPanelButtonTemplate")
-
--- Set the position and size of the button
-zoomInButton:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT")
-zoomInButton:SetWidth(30)
-zoomInButton:SetHeight(30)
-
--- Set the normal texture (the texture displayed when the button is not being clicked or highlighted)
-zoomInButton:SetNormalTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32.tga")
-zoomInButton:SetHighlightTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32.tga")
-zoomInButton:SetPushedTexture("Interface\\AddOns\\tDF\\img\\ZoomIn32.tga")
-
-
--- Set the script to run when the button is clicked
-zoomInButton:SetScript("OnClick", function()
-    MinimapZoomIn:Click()
+-- Initially hide the button
+tMinimapZoomIn:Hide()
+-- When the mouse is over the Minimap, show the button
+tMinimapZoomIn:SetScript("OnEnter", function()
+    tMinimapZoomIn:Show()
+end)
+-- When the mouse leaves the Minimap, hide the button
+tMinimapZoomIn:SetScript("OnLeave", function()
+    tMinimapZoomIn:Hide()
 end)
 ]]
+
+--Sets the Top Minimap background
+tMinimapBorderTop:SetTexture()
+
+--Move buffbars to the left of the 1.4x minimap
+tBuffButton0:ClearAllPoints()
+tBuffButton0:SetPoint("TOPRIGHT", MinimapCluster, "TOPLEFT", 10, -10)
