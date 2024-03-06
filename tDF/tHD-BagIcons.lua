e28a3a95f835f213ba2bab392462557c456862be
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-03-06 10:54:38 -0700

    Fixed bag icons, no need to reload

--- tDF/tHD-BagIcons.lua
+++ tDF/tHD-BagIcons.lua
@@ -6,26 +6,52 @@ Dragonflight.
 ]]
 -----------------Inside the bag empty slot icon-----------------
 -- Iterate through each bag (0 is backpack, 1-4 are the bag slots)
-for bag = 0, 4 do
-    -- Get the number of slots in the bag
-    local size = GetContainerNumSlots(bag)
-    -- Iterate through each slot in the bag
-    for slot = 1, size do
-        -- Get the frame for the bag slot
-        local frame = getglobal("ContainerFrame" .. bag+1 .. "Item" .. slot)
-        if frame then
-            -- Create a texture for the frame
-            local texture = frame:CreateTexture(nil, "BACKGROUND")
-            -- Set the texture to your custom image
-            texture:SetTexture("Interface\\AddOns\\tDF\\img\\bagicons.tga")
-            -- Set the size of the texture to match the size of the frame
-            texture:SetWidth(frame:GetWidth())
-            texture:SetHeight(frame:GetHeight())
-            -- Position the texture to cover the entire frame
-            texture:SetAllPoints(frame)
+-- Create a frame to listen for the PLAYER_LOGIN event
+local loginFrame = CreateFrame("Frame")
+
+-- Create a variable to track the time
+local timeSinceLastUpdate = 0
+
+-- Set the script to run when the PLAYER_LOGIN event fires
+loginFrame:SetScript("OnEvent", function()
+    -- Set the OnUpdate script
+    this:SetScript("OnUpdate", function()
+        timeSinceLastUpdate = timeSinceLastUpdate + arg1
+        if timeSinceLastUpdate < 1 then
+            return
         end
-    end
-end
+
+        -- Reset the time
+        timeSinceLastUpdate = 0
+
+        -- Remove the OnUpdate script
+        this:SetScript("OnUpdate", nil)
+
+        for bag = 0, 4 do
+            -- Get the number of slots in the bag
+            local size = GetContainerNumSlots(bag)
+            -- Iterate through each slot in the bag
+            for slot = 1, size do
+                -- Get the frame for the bag slot
+                local frame = getglobal("ContainerFrame" .. bag+1 .. "Item" .. slot)
+                if frame then
+                    -- Create a texture for the frame
+                    local texture = frame:CreateTexture(nil, "BACKGROUND")
+                    -- Set the texture to your custom image
+                    texture:SetTexture("Interface\\AddOns\\tDF\\img\\bagicons.tga")
+                    -- Set the size of the texture to match the size of the frame
+                    texture:SetWidth(frame:GetWidth())
+                    texture:SetHeight(frame:GetHeight())
+                    -- Position the texture to cover the entire frame
+                    texture:SetAllPoints(frame)
+                end
+            end
+        end
+    end)
+end)
+
+-- Register the PLAYER_LOGIN event
+loginFrame:RegisterEvent("PLAYER_LOGIN")
 -----------------Inside the bag empty slot icon-----------------
 
 -----------------Inside the bag Top Left Bag icon-----------------
