dec0f75c3d5f00c487f4fcb8b8fb134914b88c94
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-03-13 08:06:40 -0600

    Added clock to the Minimap

--- tDF/tMinimap.lua
+++ tDF/tMinimap.lua
@@ -111,3 +111,48 @@ eventFrame:SetScript("OnUpdate", function(self, elapsed)
         tBuffButton16:SetPoint("CENTER", tBuffButton0, 0, -125)
     end
 end)
+
+------------------Add Clock------------------
+MinimapClock = CreateFrame("Frame", "Clock", Minimap)
+MinimapClock:Hide()
+MinimapClock:SetFrameLevel(64)
+MinimapClock:SetPoint("CENTER", MinimapZoneText, 0, -225)
+MinimapClock:SetWidth(50)
+MinimapClock:SetHeight(23)
+MinimapClock:SetBackdrop({
+  bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+  edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+  tile = true, tileSize = 8, edgeSize = 16,
+  insets = { left = 3, right = 3, top = 3, bottom = 3 }
+})
+MinimapClock:SetBackdropBorderColor(.9,.8,.5,1)
+MinimapClock:SetBackdropColor(.4,.4,.4,1)
+
+MinimapClock:Show()
+  MinimapClock:EnableMouse(true)
+
+  MinimapClock.text = MinimapClock:CreateFontString("Status", "LOW", "GameFontNormal")
+  MinimapClock.text:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+  MinimapClock.text:SetAllPoints(MinimapClock)
+  MinimapClock.text:SetFontObject(GameFontWhite)
+  MinimapClock:SetScript("OnUpdate", function()
+    this.text:SetText(date("%H:%M"))
+  end)
+
+  MinimapClock:SetScript("OnEnter", function()
+    local h, m = GetGameTime()
+    local servertime = string.format("%.2d:%.2d", h, m)
+    local time = date("%H:%M")
+
+    GameTooltip:ClearLines()
+    GameTooltip:SetOwner(this, ANCHOR_BOTTOMLEFT)
+
+    GameTooltip:AddLine("Clock")
+    GameTooltip:AddDoubleLine("Local time", time, 1,1,1,1,1,1)
+    GameTooltip:AddDoubleLine("Server time", servertime, 1,1,1,1,1,1)
+    GameTooltip:Show()
+  end)
+
+  MinimapClock:SetScript("OnLeave", function()
+    GameTooltip:Hide()
+  end)
