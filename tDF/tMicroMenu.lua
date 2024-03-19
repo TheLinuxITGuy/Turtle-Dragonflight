508300097429c8aaaa4e60ce51b30411f0a51c68
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-03-19 11:21:46 -0600

    Added the Dragon Eye animation

--- tDF/tMicroMenu.lua
+++ tDF/tMicroMenu.lua
@@ -190,6 +190,69 @@ overlay.texture:SetAllPoints()
 overlay.texture:SetTexture("Interface\\AddOns\\tDF\\img\\uigroupfinderflipbookeye.tga")
 overlay.texture:SetTexCoord(10/512, 55/512, 8/256, 55/256)
 
+ -- Animation works, but it gets cut off before it finishes
+----------------ANIMATION----------------
+-- Define the TexCoords for each frame of the animation
+local texCoords = {
+  --512 is left and right, 256 is up and down
+  {10/512, 55/512, 8/256, 55/256}, -- 1st row
+  {74/512, 119/512, 8/256, 55/256}, --256 will stay the same as we are going right down the row
+  {138/512, 183/512, 8/256, 55/256}, -- Add 64 to all of 512
+  {202/512, 247/512, 8/256, 55/256}, -- Add 64 to all of 256
+  {266/512, 311/512, 8/256, 55/256},
+  {330/512, 375/512, 8/256, 55/256},
+  {394/512, 439/512, 8/256, 55/256},
+  {458/512, 503/512, 8/256, 55/256},
+
+  {10/512, 55/512, 72/256, 119/256}, --2nd row
+  {74/512, 119/512, 72/256, 119/256}, 
+  {138/512, 183/512, 72/256, 119/256},
+  {202/512, 247/512, 72/256, 119/256},
+  {266/512, 311/512, 72/256, 119/256},
+  {330/512, 375/512, 72/256, 119/256},
+  {394/512, 439/512, 72/256, 119/256},
+  {458/512, 503/512, 72/256, 119/256},
+
+  {10/512, 55/512, 136/256, 183/256}, -- 3rd row
+  {74/512, 119/512, 136/256, 183/256},
+  {138/512, 183/512, 136/256, 183/256},
+  {202/512, 247/512, 136/256, 183/256},
+  {266/512, 311/512, 136/256, 183/256},
+  {330/512, 375/512, 136/256, 183/256},
+  {394/512, 439/512, 136/256, 183/256},
+  {458/512, 503/512, 136/256, 183/256},
+
+  {10/512, 55/512, 200/256, 247/256},--4th row
+  {74/512, 119/512, 200/256, 247/256},
+  {138/512, 183/512, 200/256, 247/256},
+  {202/512, 247/512, 200/256, 247/256},
+  {266/512, 311/512, 200/256, 247/256}
+}
+
+-- Function to update the texture coordinates
+local currentFrame = 1
+local function UpdateTexCoords()
+  local coords = texCoords[currentFrame]
+  overlay.texture:SetTexCoord(unpack(coords))
+  currentFrame = currentFrame + 1
+  if currentFrame > table.getn(texCoords) then
+      currentFrame = 1
+  end
+end
+
+-- OnUpdate script to change the TexCoords every 0.1 seconds
+local timeSinceLastUpdate = 0
+local updateInterval = .2s5 -- Adjust this to change the speed of the animation
+overlay:SetScript("OnUpdate", function(self, elapsed)
+  local elapsed = arg1 or 0
+  timeSinceLastUpdate = timeSinceLastUpdate + elapsed
+  if timeSinceLastUpdate > updateInterval then
+      timeSinceLastUpdate = 0
+      UpdateTexCoords()
+  end
+end)
+----------------ANIMATION----------------
+
 --findGroupButton
 findGroupButton:SetScript("OnClick", function(self, button, down)
   LFT_Minimap:Show()
