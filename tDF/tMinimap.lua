41d5c79131968c707497e74992d4e2d4f5bd422c
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-31 19:18:35 -0600

    DF mail icon

--- tDF/tMinimap.lua
+++ tDF/tMinimap.lua
@@ -133,54 +133,15 @@ if pfBrowserIcon then
   pfBrowserIcon:ClearAllPoints()
   pfBrowserIcon:SetPoint("BOTTOMLEFT", UIParent, 0, 0)
 end
---[[
-------------------Add Clock------------------
-MinimapClock = CreateFrame("Frame", "Clock", Minimap)
-MinimapClock:Hide()
-MinimapClock:SetFrameLevel(64)
-MinimapClock:SetPoint("CENTER", MinimapZoneText, 0, -225)
-MinimapClock:SetWidth(68)
-MinimapClock:SetHeight(23)
-MinimapClock:SetBackdrop({
-  bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
-  edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-  tile = true, tileSize = 8, edgeSize = 16,
-  insets = { left = 3, right = 3, top = 3, bottom = 3 }
-})
-MinimapClock:SetBackdropBorderColor(.9,.8,.5,1)
-MinimapClock:SetBackdropColor(.4,.4,.4,1)
 
-MinimapClock:Show()
-MinimapClock:EnableMouse(true)
+--"You've got mail!"" -AOL 1998
+-- Define the path to your custom icon
+local customMailIcon = "Interface\\AddOns\\tDF\\img\\mail.tga"
 
-MinimapClock.text = MinimapClock:CreateFontString("Status", "LOW", "GameFontNormal")
-MinimapClock.text:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-MinimapClock.text:SetAllPoints(MinimapClock)
-MinimapClock.text:SetFontObject(GameFontWhite)
-MinimapClock:SetScript("OnUpdate", function()
-    this.text:SetText(date("%I:%M %p"))
-end)
-
-MinimapClock:SetScript("OnEnter", function()
-    local h, m = GetGameTime()
-    local servertime
-    if h > 12 then
-        servertime = string.format("%.2d:%.2d PM", h - 12, m)
-    else
-        servertime = string.format("%.2d:%.2d AM", h, m)
-    end
-    local time = date("%I:%M %p")
-
-    GameTooltip:ClearLines()
-    GameTooltip:SetOwner(this, ANCHOR_BOTTOMLEFT)
-
-    GameTooltip:AddLine("Clock")
-    GameTooltip:AddDoubleLine("Local time", time, 1,1,1,1,1,1)
-    GameTooltip:AddDoubleLine("Server time", servertime, 1,1,1,1,1,1)
-    GameTooltip:Show()
-end)
-
-MinimapClock:SetScript("OnLeave", function()
-    GameTooltip:Hide()
-end)
-]]
+--Set it
+MiniMapMailFrame:ClearAllPoints()
+MiniMapMailFrame:SetPoint("TOPRIGHT", tMinimap, "TOPRIGHT", -150, -150)
+MiniMapMailBorder:Hide()
+MiniMapMailIcon:SetTexture(customMailIcon)
+MiniMapMailIcon:SetWidth(32)
+MiniMapMailIcon:SetHeight(32)
