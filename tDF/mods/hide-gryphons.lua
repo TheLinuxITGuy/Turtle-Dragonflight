c203991640822f2a6ebc6e2982b31da14ad1b787
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-20 19:51:20 +0000

    Add files via upload

--- tDF/mods/hide-gryphons.lua
+++ tDF/mods/hide-gryphons.lua
@@ -5,6 +5,7 @@ local module = ShaguTweaks:register({
   title = T["Hide Gryphons"],
   description = T["Hides the gryphons left and right of the action bar."],
   expansions = { ["vanilla"] = true, ["tbc"] = true },
+  category = T["Action Bar"],
   enabled = nil,
 })
 
