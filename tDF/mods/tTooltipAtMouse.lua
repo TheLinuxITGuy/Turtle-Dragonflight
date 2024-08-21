c68a3514789ae39f8647a09f618893053a6c46c1
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-21 13:51:43 -0600

    Add files via upload

--- tDF/mods/tTooltipAtMouse.lua
+++ tDF/mods/tTooltipAtMouse.lua
@@ -11,14 +11,31 @@ local module = ShaguTweaks:register({
 
 module.enable = function(self)
 
-  local function UpdateTooltipPosition()
-    local x, y = GetCursorPosition()
-    local scale = UIParent:GetEffectiveScale()
-    GameTooltip:ClearAllPoints()
-    GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", x / scale, y / scale)
-  end
+local function UpdateTooltipPosition()
+  local x, y = GetCursorPosition()
+  local scale = UIParent:GetEffectiveScale()
+  GameTooltip:ClearAllPoints()
+  GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", x / scale, y / scale)
+end
+
+local function ShowTooltipUntilMouseMove()
+  local initialX, initialY = GetCursorPosition()
+  local scale = UIParent:GetEffectiveScale()
+  local padding = 50
+  GameTooltip:SetScript("OnUpdate", function()
+    local currentX, currentY = GetCursorPosition()
+    if math.abs(currentX - initialX) > padding or math.abs(currentY - initialY) > padding then
+      GameTooltip:Hide()
+      GameTooltip:SetScript("OnUpdate", nil)
+    else
+      GameTooltip:ClearAllPoints()
+      GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", initialX / scale, initialY / scale)
+    end
+  end)
+end
 
 GameTooltip:SetOwner(UIParent, "ANCHOR_CURSOR")
 GameTooltip:SetScript("OnUpdate", UpdateTooltipPosition)
+GameTooltip:SetScript("OnShow", ShowTooltipUntilMouseMove)
 
 end
