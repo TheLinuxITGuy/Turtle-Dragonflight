f733cbb2b8d28c5de15d134f47455060bb4fecf6
edizmetin <ediz1995@gmail.com>
2024-09-06 11:48:31 +0300

    Update tTooltipAtMouse.lua
    Enable item comparison when tooltip at mouse module is enabled.

--- tDF/mods/tTooltipAtMouse.lua
+++ tDF/mods/tTooltipAtMouse.lua
@@ -10,32 +10,41 @@ local module = ShaguTweaks:register({
   })
 
 module.enable = function(self)
-
-local function UpdateTooltipPosition()
-  local x, y = GetCursorPosition()
-  local scale = UIParent:GetEffectiveScale()
-  GameTooltip:ClearAllPoints()
-  GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", x / scale, y / scale)
-end
-
-local function ShowTooltipUntilMouseMove()
-  local initialX, initialY = GetCursorPosition()
-  local scale = UIParent:GetEffectiveScale()
-  local padding = 50
-  GameTooltip:SetScript("OnUpdate", function()
-    local currentX, currentY = GetCursorPosition()
-    if math.abs(currentX - initialX) > padding or math.abs(currentY - initialY) > padding then
-      GameTooltip:Hide()
-      GameTooltip:SetScript("OnUpdate", nil)
-    else
-      GameTooltip:ClearAllPoints()
-      GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", initialX / scale, initialY / scale)
+    local function UpdateTooltipPosition()
+        local x, y = GetCursorPosition()
+        local scale = UIParent:GetEffectiveScale()
+        GameTooltip:ClearAllPoints()
+        GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", x / scale, y / scale)
     end
-  end)
+
+    local function ShowTooltipUntilMouseMove()
+        local initialX, initialY = GetCursorPosition()
+        local scale = UIParent:GetEffectiveScale()
+        local padding = 50
+
+        GameTooltip:SetScript("OnUpdate", function()
+            local currentX, currentY = GetCursorPosition()
+            if math.abs(currentX - initialX) > padding or math.abs(currentY - initialY) > padding then
+                GameTooltip:Hide()
+                GameTooltip:SetScript("OnUpdate", nil)
+            else
+                GameTooltip:ClearAllPoints()
+                GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", initialX / scale, initialY / scale)
+            end
+        end)
+    end
+
+    local function OnTooltipShow()
+        if IsShiftKeyDown() then
+            -- Allow the Equip Compare module to show comparisons
+            tDF.eqcompare.GameTooltipShow(GameTooltip)
+        end
+        ShowTooltipUntilMouseMove() -- Continue to update position based on mouse movement
+    end
+
+    -- Set tooltip to follow the mouse cursor
+    GameTooltip:SetOwner(UIParent, "ANCHOR_CURSOR")
+    GameTooltip:SetScript("OnUpdate", UpdateTooltipPosition)
+    GameTooltip:SetScript("OnShow", OnTooltipShow)
 end
 
-GameTooltip:SetOwner(UIParent, "ANCHOR_CURSOR")
-GameTooltip:SetScript("OnUpdate", UpdateTooltipPosition)
-GameTooltip:SetScript("OnShow", ShowTooltipUntilMouseMove)
-
-end
